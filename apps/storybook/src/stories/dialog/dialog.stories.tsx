e3a8e3fab1fbe8979da264778bffff2c1e6914e7
priosshrsth <shrsthprios@hotmail.com>
2025-11-07 04:30:32 +0000

    update dialog stories

--- apps/storybook/src/stories/dialog/dialog.stories.tsx
+++ apps/storybook/src/stories/dialog/dialog.stories.tsx
@@ -10,25 +10,18 @@ function DialogComponent(props: Omit<React.ComponentProps<typeof Dialog>, "open"
       <button onClick={() => setOpen(true)} type="button">
         Open
       </button>
-      <Dialog onClose={() => setOpen(false)} open={_open} {...props} />
+      <Dialog onClose={() => setOpen(false)} {...props} open={_open} />
     </div>
   );
 }
 
-const meta = preview.meta({ component: DialogComponent });
+const meta = preview.meta({ component: DialogComponent, title: "Dialog", tags: ["autodocs"] });
 
 export default meta;
 
 export const Primary = meta.story({
   args: {
     children: "This is a dialog",
-  },
-});
-
-export const OpenByDefault = meta.story({
-  args: {
-    children: "This is a dialog",
-    open: true,
   },
 });
 
